5fb549a3138070438b0c1f77a225f0b0111e1aac
Oleksandr Levytskii <alex.leo.kyky@gmail.com>
2020-12-14 19:26:14 +0200

    ability to update when select options changes

--- jQuery.select2Buttons.js
+++ jQuery.select2Buttons.js
@@ -12,66 +12,80 @@
  * Licensed under the MIT
  **/
 jQuery.fn.select2Buttons = function(options) {
-  return this.each(function(){
-    var $ = jQuery;
-    var select = $(this);
-    var multiselect = select.attr('multiple');
-    select.hide();
+    return this.each(function(){
+        var $ = jQuery;
+        var select = $(this);
+        var multiselect = select.attr('multiple');
+        var id;
+        id = select.data('button-id');
+        var buttonsHtml;
 
-    var buttonsHtml = $('<div class="select2Buttons"></div>');
-    var selectIndex = 0;
-    var addOptGroup = function(optGroup){
-      if (optGroup.attr('label')){
-        buttonsHtml.append('<strong>' + optGroup.attr('label') + '</strong>');
-      }
-      var ulHtml =  $('<ul class="select-buttons">');
-      optGroup.children('option').each(function(){
-        var liHtml = $('<li></li>');
-        if ($(this).attr('disabled') || select.attr('disabled')){
-          liHtml.addClass('disabled');
-          liHtml.append('<span>' + $(this).html() + '</span>');
+        if(typeof id === 'undefined') {
+            id = Math.round(Math.random()*100000);
+            select.data('button-id', id);
+            buttonsHtml = $('<div class="select2Buttons" id="'+ id+'"></div>');
+        } else {
+            buttonsHtml = $('#'+ id);
+            buttonsHtml.empty();
+        }
+        select.hide();
+
+        var selectIndex = 0;
+        var addOptGroup = function(optGroup){
+            if (optGroup.attr('label')){
+                buttonsHtml.append('<strong>' + optGroup.attr('label') + '</strong>');
+            }
+            var ulHtml =  $('<ul class="select-buttons">');
+            optGroup.children('option').each(function(){
+                var liHtml = $('<li></li>');
+                if ($(this).attr('disabled') || select.attr('disabled')){
+                    liHtml.addClass('disabled');
+                    liHtml.append('<span>' + $(this).html() + '</span>');
+                }else{
+                    liHtml.append('<a href="#" data-select-index="' + selectIndex + '">' + $(this).html() + '</a>');
+                }
+
+                // Mark current selection as "picked"
+                if((!options || !options.noDefault) && $(this).attr('selected')){
+                    liHtml.children('a, span').addClass('picked');
+                }
+                ulHtml.append(liHtml);
+                selectIndex++;
+            });
+            buttonsHtml.append(ulHtml);
+        }
+
+        var optGroups = select.children('optgroup');
+        if (optGroups.length == 0) {
+            addOptGroup(select);
         }else{
-          liHtml.append('<a href="#" data-select-index="' + selectIndex + '">' + $(this).html() + '</a>');
+            optGroups.each(function(){
+                addOptGroup($(this));
+            });
+        }
+        if(!buttonsHtml.hasClass('initialized')) {
+            buttonsHtml.addClass('initialized');
+            select.after(buttonsHtml);
         }
 
-        // Mark current selection as "picked"
-        if((!options || !options.noDefault) && $(this).attr('selected')){
-          liHtml.children('a, span').addClass('picked');
-        }
-        ulHtml.append(liHtml);
-        selectIndex++;
-      });
-      buttonsHtml.append(ulHtml);
-    }
 
-    var optGroups = select.children('optgroup');
-    if (optGroups.length == 0) {
-      addOptGroup(select);
-    }else{
-      optGroups.each(function(){
-        addOptGroup($(this));
-      });
-    }
-
-    select.after(buttonsHtml);
-
-    buttonsHtml.find('a').click(function(e){
-      e.preventDefault();
-      var clickedOption = $(select.find('option')[$(this).attr('data-select-index')]);
-      if(multiselect){
-        if(clickedOption.attr('selected')){
-          $(this).removeClass('picked');
-          clickedOption.removeAttr('selected');
-        }else{
-          $(this).addClass('picked');
-          clickedOption.attr('selected', 'selected');
-        }
-      }else{
-        buttonsHtml.find('a, span').removeClass('picked');
-        $(this).addClass('picked');
-        clickedOption.attr('selected', 'selected');
-      }
-      select.trigger('change');
+        buttonsHtml.find('a').click(function(e){
+            e.preventDefault();
+            var clickedOption = $(select.find('option')[$(this).attr('data-select-index')]);
+            if(multiselect){
+                if(clickedOption.attr('selected')){
+                    $(this).removeClass('picked');
+                    clickedOption.removeAttr('selected');
+                }else{
+                    $(this).addClass('picked');
+                    clickedOption.attr('selected', 'selected');
+                }
+            }else{
+                buttonsHtml.find('a, span').removeClass('picked');
+                $(this).addClass('picked');
+                clickedOption.attr('selected', 'selected');
+            }
+            select.trigger('change');
+        });
     });
-  });
 };
